2d9a261d4d7e4868ee91c082c02bea6f923d3332
Ricardo Figueiredo Camargo <ricardofigueiredocamargo@gmail.com>
2023-05-10 10:03:47 -0300

    feat: manipulando nomes

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
 
 function App() {
+  const [firstName, setFirstName] = useState(() => {
+    const name = localStorage.getItem("FIRST-NAME")
+    if (name == null) return ''
+
+    return name
+  })
+
+  const [lastName, setLastName] = useState(() => {
+    const name = localStorage.getItem("LAST-NAME")
+    if (name == null) return ''
+
+    return name
+  })
+
+  const fullName = firstName + ' ' + lastName
+
+  useEffect(() => {
+    localStorage.setItem("FIRST-NAME", firstName)
+  }, [firstName])
+
+  useEffect(() => {
+    localStorage.setItem("LAST-NAME", lastName)
+  }, [lastName])
+
+  function handleFirstName(e) {
+    setFirstName(e.target.value)
+  }
+
+  function handleLastName(e) {
+    setLastName(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <label htmlFor='iname'>Your Fist Name: </label>
+        <input type='text' id='iname' value={firstName} onChange={handleFirstName} />
+      </div>  
+      <div>
+        <label htmlFor='iname'>Your Last Name: </label>
+        <input type='text' id='iname' value={lastName} onChange={handleLastName} />
+      </div>
+      <h1>Hello, {fullName}!</h1>
+    </>
   );
 }
 
